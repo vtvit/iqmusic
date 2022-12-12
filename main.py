3df6df7775a948bdcb0ba34375a13d08b877f1b4
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-12-12 12:38:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from Musicjepthon.التشغيل import arq
+from Musicjepthon.کارپێکردن import arq
 
         
 async def main():
@@ -9,7 +9,7 @@ async def main():
     print(
         """
     ------------------
-   | تم تشغيل ميوزك الجوكر |
+   | میوزیکی بۆتی زیرەك کاردەکات |
     ------------------
 """
     )
