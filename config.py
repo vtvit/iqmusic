fb5d5bb88fa7f78996101477c28c974589923a01
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-12-14 01:55:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -22,5 +22,5 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Musicjepthon"))
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="vtvit"))
 call_py = PyTgCalls(bot)
